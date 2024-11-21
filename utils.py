a3236c57fe33248438a51652337642e736762737
Aris Tsakpinis <aris.tsakpinis@googlemail.com>
2024-11-21 18:27:55 +0000

    update lab 06 and utils.py

--- utils.py
+++ utils.py
@@ -1,24 +1,26 @@
 import pandas as pd
 import boto3
+import pickle
+
 from collections import Counter
 from langchain_core.tools import tool
-from langchain_aws import ChatBedrockConverse, ChatBedrock
-from langchain_community.document_loaders import PyPDFDirectoryLoader
+from langchain_core.runnables.config import RunnableConfig
+from langchain_aws import ChatBedrockConverse
 from langchain_text_splitters import RecursiveCharacterTextSplitter
-from langchain_chroma import Chroma
 from langchain_aws.embeddings.bedrock import BedrockEmbeddings
 from langgraph.prebuilt import create_react_agent
 from langgraph.checkpoint.memory import MemorySaver
+from langchain.tools.retriever import create_retriever_tool
+from langchain_community.vectorstores import FAISS
+from langchain.retrievers import ParentDocumentRetriever
 
-from langchain_core.messages import HumanMessage as LCHumanMessage
-from langchain_core.messages import AIMessage as LCAIMessage
-from langchain_core.messages import ToolMessage as LCToolMessage
 
-from ragas.messages import Message as RGMessage
 from ragas.messages import HumanMessage as RGHumanMessage
 from ragas.messages import AIMessage as RGAIMessage
 from ragas.messages import ToolMessage as RGToolMessage
 from ragas.messages import ToolCall as RGToolCall
+
+from io import BytesIO
 
 def convert_message_langchain_to_ragas(lc_message):
     message_dict = lc_message.model_dump()
@@ -29,8 +31,13 @@ def convert_message_langchain_to_ragas(lc_message):
             text = list(filter((lambda x: x['type'] == 'text'), message_dict['content']))
             tool = list(filter((lambda x: x['type'] == 'tool_use'), message_dict['content']))
             if len(text) > 0 and len(tool) > 0:
-                rg_message = RGAIMessage(content=text[0]['text'], tool_calls=[RGToolCall(name=tool[0]['name'], args={#'id': tool[0]['id'], 
-                                                                                                                    'query': tool[0]['input'][list(tool[0]['input'].keys())[0]]})])
+
+                if len(list(tool[0]['input'].keys())) > 0:
+                    dyn_args = {'query': tool[0]['input'][list(tool[0]['input'].keys())[0]]}
+                else: 
+                    dyn_args = {}
+                
+                rg_message = RGAIMessage(content=text[0]['text'], tool_calls=[RGToolCall(name=tool[0]['name'], args= dyn_args)])
             elif len(text) > 0:
                 rg_message = RGAIMessage(content=text[0]['text'])
             elif len(tool) > 0:
@@ -45,7 +52,7 @@ def convert_message_langchain_to_ragas(lc_message):
 
 def create_agent(enable_memory = False):
     # ---- ⚠️ Update region for your AWS setup ⚠️ ----
-    bedrock_client = boto3.client("bedrock-runtime", region_name="us-west-2")
+    bedrock_client = boto3.client("bedrock-runtime", region_name="us-east-1")
     
     
     
@@ -64,53 +71,34 @@ def create_agent(enable_memory = False):
             return df
         except FileNotFoundError:
             return pd.DataFrame(
-                columns=[
-                    "Name",
-                    "Current_Location",
-                    "Age",
-                    "Past_Travel_Destinations",
-                    "Number_of_Trips",
-                    "Flight_Number",
-                    "Departure_City",
-                    "Arrival_City",
-                    "Flight_Date",
-                ]
+                columns=["Id", "Name","Current_Location","Age","Past_Travel_Destinations", "Number_of_Trips", "Flight_Number", "Departure_City","Arrival_City","Flight_Date",]
             )
     
     
     @tool
-    def compare_and_recommend_destination(name: str) -> str:
-        """This tool is used to check which destinations user has already traveled. 
-        Use name of the user to fetch the information about that user.
-        If user has already been to a city then do not recommend that city. 
-        
-        Args:
-            name (str): Name of the user.
+    def compare_and_recommend_destination(config: RunnableConfig) -> str:
+        """This tool is used to check which destinations user has already traveled.
+        If user has already been to a city then do not recommend that city.
+    
         Returns:
             str: Destination to be recommended.
-        
+    
         """
     
         df = read_travel_data()
+        user_id = config.get("configurable", {}).get("user_id")
     
-        if name not in df["Name"].values:
+        if user_id not in df["Id"].values:
             return "User not found in the travel database."
     
-        user_data = df[df["Name"] == name].iloc[0]
+        user_data = df[df["Id"] == user_id].iloc[0]
         current_location = user_data["Current_Location"]
         age = user_data["Age"]
         past_destinations = user_data["Past_Travel_Destinations"].split(", ")
     
         # Get all past destinations of users with similar age (±5 years) and same current location
-        similar_users = df[
-            (df["Current_Location"] == current_location)
-            & (df["Age"].between(age - 5, age + 5))
-        ]
-        all_destinations = [
-            dest
-            for user_dests in similar_users["Past_Travel_Destinations"].str.split(", ")
-            for dest in user_dests
-        ]
+        similar_users = df[(df["Current_Location"] == current_location) & (df["Age"].between(age - 5, age + 5))]
+        all_destinations = [dest for user_dests in similar_users["Past_Travel_Destinations"].str.split(", ") for dest in user_dests ]
     
         # Count occurrences of each destination
         destination_counts = Counter(all_destinations)
@@ -129,20 +117,30 @@ def create_agent(enable_memory = False):
         return f"Based on your current location ({current_location}), age ({age}), and past travel data, we recommend visiting {recommended_destination}."
     
     
-    loader = PyPDFDirectoryLoader("data/us")
-    docs = loader.load()
-    
-    text_splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=200)
-    splits = text_splitter.split_documents(docs)
-    
-    
     embeddings_model = BedrockEmbeddings(
         client=bedrock_client, model_id="amazon.titan-embed-text-v1"
     )
-    vectorstore = Chroma.from_documents(documents=splits, embedding=embeddings_model)
-    retriever = vectorstore.as_retriever()
     
-    from langchain.tools.retriever import create_retriever_tool
+    child_splitter = RecursiveCharacterTextSplitter(
+        separators=["\n", "\n\n"], chunk_size=2000, chunk_overlap=250
+    )
+    
+    in_memory_store_file = "data/section_doc_store.pkl"
+    vector_store_file = "data/section_vector_store.pkl"
+    
+    store = pickle.load(open(in_memory_store_file, "rb"))
+    vector_db_buff = BytesIO(pickle.load(open(vector_store_file, "rb")))
+    vector_db = FAISS.deserialize_from_bytes(
+        serialized=vector_db_buff.read(),
+        embeddings=embeddings_model,
+        allow_dangerous_deserialization=True,
+    )
+
+    retriever = ParentDocumentRetriever(
+        vectorstore=vector_db,
+        docstore=store,
+        child_splitter=child_splitter,
+    )    
     
     retriever_tool = create_retriever_tool(
         retriever,
